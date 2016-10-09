8df5590f478c1523a5fb7e09043b9b1598580af9
katharine benefico <kbenefico@gmail.com>
2016-10-09 15:18:58 -0400

    add routes

--- client/index.jsx
+++ client/index.jsx
@@ -1,6 +1,18 @@
-// require('dotenv').config();
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 import App from './components/App.jsx';
+import UserAuth from './components/UserAuth.jsx';
 
 ReactDOM.render(<App />, document.querySelector('#root'));
+
+ReactDOM.render((
+  <Router history={hashHistory} >
+    <Route path="/" component={App} >
+      <Route path="login" component={UserAuth} />
+    </Route>
+
+  </Router>
+  ), document.querySelector('#root'));
+
+
