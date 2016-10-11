71bacbcbe49653af4ef7038b574618b2b1be7219
Jake Hamilton <hamiltonjm12@gmail.com>
2016-10-11 16:45:31 -0400

    Create playlist and track controllers and routers, start building routes and basic call methods

---
+++ app/controllers/TrackController.js
@@ -0,0 +1,16 @@
+// const TrackDAO = require('../services/TrackDAO.js');
+
+class TrackController {
+  static getTrackById (req, res) {
+    // TrackDAO.findById({ id: req.params.id })
+    //         .then((track) => {
+    //           res.status(200).json(track);
+    //         });
+  }
+
+  static saveTrack (req, res) {
+    // const { title, artist, preview } = req.body;
+    // TrackDAO.createTrack({ title, artist, preview });
+  }
+}
+
