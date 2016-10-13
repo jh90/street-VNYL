2d292c27499120f22ef69287fe4d548a09cfda34
katharine benefico <kbenefico@gmail.com>
2016-10-13 15:30:48 -0400

    add playlist and search modals wip

--- client/components/SpotifyTest.jsx
+++ client/components/SpotifyTest.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import request from 'superagent';
-
+import Modal from 'react-modal';
 import TrackListElement from './TrackListElement.jsx';
+
+const propTypes = {
+  openSearchModal: React.PropTypes.func,
+  closeSearchModal: React.PropTypes.func,
+  afterOpenSearchModal: React.PropTypes.func,
+};
 
 export default class SpotifyTest extends React.Component {
   constructor () {
@@ -9,13 +15,18 @@ export default class SpotifyTest extends React.Component {
     this.state = {
       keyword: '',
       tracks: [],
-    }
+      searchModalOpen: false,
+    };
+
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.getTracks = this.getTracks.bind(this);
+    this.openSearchModal = this.openSearchModal.bind(this);
+    this.closeSearchModal = this.closeSearchModal.bind(this);
+    this.afterOpenSearchModal = this.afterOpenSearchModal.bind(this);
   }
 
-  cleanTrackData (data) {
+  cleanTrackData(data) {
     const cleanTrackObject = {
       title: data.name,
       artist: data.artists[0].name,
@@ -51,30 +62,56 @@ export default class SpotifyTest extends React.Component {
   handleSubmit (e) {
     e.preventDefault();
     this.getTracks();
+    this.openSearchModal();
   }
 
+  openSearchModal() {
+    this.setState({ searchModalOpen: true });
+  }
+
+  afterOpenSearchModal() {
+    this.refs.subtitle.style.color = '#f00';
+  }
+
+  closeSearchModal() {
+    this.setState({ searchModalOpen: false });
+  }
   render () {
     return (
       <div>
-        <form id='search-form' onSubmit={this.handleSubmit}>
-          <input type='text'
-                 name='keyword'
-                 placeholder='track'
-                 value={this.state.keyword}
-                 onChange={this.handleChange} />
-          <input id='searchButton' type='submit'
-                 onClick={this.handleSubmit}
-                 value='Search'
-              />
+        <form id="search-form" onSubmit={this.handleSubmit}>
+          <input
+            id="searchText"
+            type="text"
+            name="keyword"
+            placeholder="track"
+            value={this.state.keyword}
+            onChange={this.handleChange}
+          />
+          <input
+            id="searchButton"
+            type="submit"
+            onClick={this.handleSubmit}
+            value="Search"
+          />
         </form>
-        <ul id="song-list">
-        {this.state.tracks.map((track) => {
-          return (
-            <TrackListElement trackData={track} />
-          );
-        })}
-        </ul>
+        <Modal
+          className="search-modal"
+          isOpen={this.state.searchModalOpen}
+          onAfterOpen={this.afterOpenSearchModal}
+          onRequestClose={this.closeSearchModal}
+        >
+          <ul id="song-list">
+            {this.state.tracks.map((track) => {
+              return (
+                <TrackListElement trackData={track} />
+              );
+            })}
+          </ul>
+        </Modal>
       </div>
     );
   }
 }
+
+SpotifyTest.propTypes = propTypes;
--- client/components/TrackListElement.jsx
+++ client/components/TrackListElement.jsx
@@ -7,13 +7,29 @@ const propTypes = {
 export default class TrackListElement extends React.Component {
   render () {
     return (
-      <ul>
-        <li>{this.props.trackData.title}</li>
-        <li>{this.props.trackData.artist}</li>
-        <li>{this.props.trackData.previewURL}</li>
-      </ul>
+      <div id="track-element">
+        <ul id="artist-title-list">
+          <li id="song-title">"{this.props.trackData.title}"</li>
+          <li id="artist-name">{this.props.trackData.artist}</li>
+        </ul>
+        <audio controls>
+          <source src={this.props.trackData.previewURL} type="audio/mpeg" />
+        </audio>
+      </div>
     );
   }
 }
 
 TrackListElement.propTypes = propTypes;
+
+
+// <li>{this.props.trackData.previewURL}</li>
+
+// <a href="http://www.w3schools.com">
+// <img border="0" alt="W3Schools" src="logo_w3s.gif" width="100" height="100">
+// </a>
+
+// <a href="#">Locations</a>
+
+// <a href="#" id="hamburger"><img src="../images/hamburger.png" /></a>
+// <a href={this.props.trackData.previewURL}><img src="../images/playButton.png"/> </a>
